a6c5b35394e7617d69abc8fe3e8ba18d309862fa
Matthew Waters <matthew@centricular.com>
2022-02-16 19:24:54 +1100

    fuzz: add fuzzer for candidate parsing

---
+++ fuzz/fuzz_targets/parse_candidate.rs
@@ -0,0 +1,33 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+use once_cell::sync::Lazy;
+
+#[macro_use]
+extern crate tracing;
+use tracing_subscriber::EnvFilter;
+
+use librice::candidate::*;
+
+use std::str::FromStr;
+
+#[derive(arbitrary::Arbitrary, Debug)]
+struct Data<'data> {
+    data: &'data str,
+}
+
+pub fn debug_init() {
+    static TRACING: Lazy<()> = Lazy::new(|| {
+        if let Ok(filter) = EnvFilter::try_from_default_env() {
+            tracing_subscriber::fmt().with_env_filter(filter).init();
+        }
+    });
+
+    Lazy::force(&TRACING);
+}
+
+fuzz_target!(|data: Data| {
+    debug_init();
+    let res = Candidate::from_str(data.data);
+    debug!("candidate result {:?}", res);
+});
