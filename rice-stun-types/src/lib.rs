cf5dbc1d9686a6c80f19e8c93fba8ad22d6589b3
Tim-Philipp Müller <tim@centricular.com>
2026-02-05 13:55:00 +0000

    Add SPDX-License-Identifiers

--- rice-stun-types/src/lib.rs
+++ rice-stun-types/src/lib.rs
@@ -5,6 +5,8 @@
 // <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
 
 #![deny(missing_debug_implementations)]
 #![deny(missing_docs)]
