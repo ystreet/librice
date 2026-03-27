ce748ad0bd6c2bd3d9ed4f48f1ce2f0e6efd6ac1
Matthew Waters <matthew@centricular.com>
2026-03-27 16:36:06 +1100

    librice: add example usage of the agent at the crate root

--- librice/src/lib.rs
+++ librice/src/lib.rs
@@ -54,6 +54,112 @@
 //! [RFC8445]: <https://tools.ietf.org/html/rfc8445>
 //! [rice-proto]: <https://docs.rs/rice-proto>
 //! [rice-c]: <https://docs.rs/rice-c>
+//!
+//! ## Example
+//!
+//! ```
+//! # #[cfg(feature = "runtime-tokio")]
+//! # let runtime = tokio::runtime::Builder::new_current_thread()
+//! #     .enable_all()
+//! #     .build()
+//! #     .unwrap();
+//! # #[cfg(feature = "runtime-tokio")]
+//! # let _runtime = runtime.enter();
+//! # let task = async move {
+//! use core::net::SocketAddr;
+//! use futures::stream::StreamExt;
+//! use librice::agent::{Agent, AgentMessage};
+//! use librice::stream::Credentials;
+//! use librice::candidate::{Candidate, CandidateType, TransportType};
+//!
+//! let agent = Agent::default();
+//! // Configure the agent as you wish.
+//! // e.g. add a stun server.
+//! // agent.add_stun_server(TransportType::Udp, SocketAddr::new([192, 168, 0, 1], 3478));
+//!
+//! // Add a stream and component within that stream for data flow.
+//! let stream = agent.add_stream();
+//! let local_credentials = Credentials::new("luser", "lpass");
+//! stream.set_local_credentials(&local_credentials);
+//! let component = stream.add_component().unwrap();
+//!
+//! // At some point you will also need the remote credentials to be able to successfully connect
+//! // with the peer. If trickle-ice, then this can occur during candidate gathering, otherwise,
+//! // should occur before the remote candidates are added to the agent.
+//! let remote_credentials = Credentials::new("ruser", "rpass");
+//! stream.set_local_credentials(&remote_credentials);
+//!
+//! // Retrieve the receive end of a message queue that indicates gathering state, component state,
+//! // and other such messages.
+//! let mut messages = agent.messages();
+//!
+//! // start gathering candidates.
+//! stream.gather_candidates().await.unwrap();
+//!
+//! while let Some(msg) = messages.next().await {
+//!    match msg {
+//!        AgentMessage::GatheredCandidate(stream, gathered) => {
+//!             // based on local policy, you can choose to never add the locally gathered
+//!             // candidate to the stream to avoid using a candidate for connectivity checks later.
+//!             stream.add_local_gathered_candidate(gathered);
+//!             // For trickle-ice handling, you would send the gathered candidate to the peer.
+//!         }
+//!         AgentMessage::GatheringComplete(component) => {
+//!             // For non trickle-ice handling, if all relevant components in a stream have
+//!             // completed gathering, you would retrieve the list of local candidates
+//!             // and send that to the peer along with any other setup information required.
+//!             println!("component {} has completed gathering", component.id());
+//!             let stream = component.stream();
+//!             for cand in stream.local_candidates() {
+//!                 println!(
+//!                     "stream {} has gathered local candidate {}",
+//!                     stream.id(),
+//!                     cand.to_sdp_string()
+//!                 );
+//!             }
+//!             break;
+//!         }
+//!         AgentMessage::ComponentStateChange(component, new_state) => {
+//!             println!("component {} has changed state to {new_state:?}", component.id());
+//!         },
+//!    }
+//! }
+//!
+//! // On receiving remote candidates from the peer you would add them to the agent in order to
+//! // start connectivity checks.
+//! # let remote_host_addr = SocketAddr::new([127, 0, 0, 1].into(), 54321);
+//! # let remote_candidate = Candidate::builder(
+//! #     1, // component id
+//! #     CandidateType::Host,
+//! #     TransportType::Udp,
+//! #     "0", // foundation
+//! #     remote_host_addr.into(),
+//! # )
+//! # .build();
+//! stream.add_remote_candidate(&remote_candidate);
+//!
+//! // Once the complete set of remote candidates have been received, then notify the agent of
+//! // this.
+//! stream.end_of_remote_candidates();
+//!
+//! // connectivity checks will progress and either a successful pair (the selected pair) will be
+//! // found, or failure will be signaled for the component's connection state.
+//! // while let Some(msg) = messages.next().await {
+//! //     match msg {
+//! //         ...
+//! //     }
+//! // }
+//!
+//! // once a selected pair is chosen, data can be received.
+//! let recv = component.recv();
+//! // and sent
+//! component.send([1, 2, 3, 4].as_slice()).await;
+//! # };
+//! # #[cfg(feature = "runtime-smol")]
+//! # smol::block_on(task);
+//! # #[cfg(all(not(feature = "runtime-smol"), feature = "runtime-tokio"))]
+//! # runtime.block_on(task);
+//! ```
 
 pub mod agent;
 pub mod component;
